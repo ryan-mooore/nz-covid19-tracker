56fefa182ac69903b664d01a57e379666c671bae
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-08-23 17:10:49 +1200

    simplify

--- src/tweets/COVID-19: Current cases/infection_update.py
+++ src/tweets/COVID-19: Current cases/infection_update.py
@@ -2,64 +2,31 @@ from typing import Union
 
 
 def tweet(data: dict) -> Union[bool, list[str]]:
-    if not data["pages"]["COVID-19: Current cases"]["Current situation"][
-        "Change in last 24 hours"
-    ]["Active"]:
+
+    infected = (
+        data["pages"]["COVID-19: Current cases"]["Current situation"][
+            "Change in last 24 hours"
+        ]["Active"]
+        + data["pages"]["COVID-19: Current cases"]["Current situation"][
+            "Change in last 24 hours"
+        ]["Recovered"]
+        + data["pages"]["COVID-19: Current cases"]["Current situation"][
+            "Change in last 24 hours"
+        ]["Deceased"]
+    )
+
+    if not infected:
         print("No new active cases in past 24 hours")
         return False
 
-    history = data["history"]["COVID-19: Current cases"]["Case details"][
-        "Change in last 24 hours"
-    ]
-    current = data["pages"]["COVID-19: Current cases"]["Case details"][
-        "Change in last 24 hours"
-    ]
-    infected = data["pages"]["COVID-19: Current cases"]["Current situation"][
-        "Change in last 24 hours"
-    ]["Active"]
     active = data["pages"]["COVID-19: Current cases"]["Current situation"]["Total"][
         "Active"
     ]
 
-    def compare(keys: Union[str, list[str]]) -> int:
-        if type(keys) is str:
-            keys = [keys]
-        total = 0
-        for key in keys:
-            total += current[key] - history[key]
-        return total
-
-    border = compare(
-        [
-            "People who travelled internationally and were diagnosed in managed facilities at the border",
-            "People in close contact with someone who caught COVID-19 while overseas",
-        ]
-    )
-    community = compare("Caught COVID-19 from someone locally")
-    unknown = compare(
-        ["Caught COVID-19 within NZ, but source is unknown", "Under investigation"]
-    )
-
-    total_str: list[str] = []
-    if border > 0:
-        total_str.append(f"🛬 From the border: {border}")
-    if community > 0:
-        total_str.append(
-            f"🏡 In the community: {community}",
-        )
-    if unknown > 0:
-        total_str.append(
-            f'🔎 Under investigation: -{current["Caught COVID-19 within NZ, but source is unknown"] + current["Under investigation"]}'
-        )
-
-    if border + community + unknown == active:
-        return [
-            "🦠 INFECTION UPDATE",
-            str(infected)
-            + " people have been infected from COVID-19 in the past 24 hours, bringing the total active cases to "
-            + str(active)
-            + ":",
-        ] + total_str
-    else:
-        print("Error: infected number did not equal current - history")
-        return False
+    return [
+        "🦠 INFECTION UPDATE",
+        str(infected)
+        + (" people have " if infected > 1 else " person has ")
+        + "been infected from COVID-19 in the past 24 hours, bringing the total active cases to "
+        + str(active),
+    ]
--- src/tweets/COVID-19: Current cases/recovery_update.py
+++ src/tweets/COVID-19: Current cases/recovery_update.py
@@ -2,55 +2,27 @@ from typing import Union
 
 
 def tweet(data: dict) -> Union[bool, list[str]]:
-    if not data["pages"]["COVID-19: Current cases"]["Current situation"][
-        "Change in last 24 hours"
-    ]["Recovered"]:
-        print("No recovered cases in past 24 hours")
-        return False
 
-    history = data["history"]["COVID-19: Current cases"]["Case details"][
-        "Change in last 24 hours"
-    ]
-    current = data["pages"]["COVID-19: Current cases"]["Case details"][
-        "Change in last 24 hours"
-    ]
     recovered = data["pages"]["COVID-19: Current cases"]["Current situation"][
         "Change in last 24 hours"
     ]["Recovered"]
+
+    if not recovered:
+        print("No new active cases in past 24 hours")
+        return False
+
     active = data["pages"]["COVID-19: Current cases"]["Current situation"]["Total"][
         "Active"
     ]
 
-    def compare(keys: Union[str, list[str]]) -> int:
-        if type(keys) is str:
-            keys = [keys]
-        total = 0
-        for key in keys:
-            total += current[key] - history[key]
-        return total
+    return [
+        "🦠 RECOVERY UPDATE",
+        str(recovered)
+        + (" people have " if recovered > 1 else " person has ")
+        + "been infected from COVID-19 in the past 24 hours, bringing the total active cases to "
+        + str(active),
+    ]
 
-    border = compare(
-        [
-            "People who travelled internationally and were diagnosed in managed facilities at the border",
-            "People in close contact with someone who caught COVID-19 while overseas",
-        ]
-    )
-    community = compare("Caught COVID-19 from someone locally")
-    unknown = compare(
-        ["Caught COVID-19 within NZ, but source is unknown", "Under investigation"]
-    )
-
-    total_str: list[str] = []
-    if border < 0:
-        total_str.append(f"🛬 From the border: {border}")
-    if community < 0:
-        total_str.append(
-            f"🏡 In the community: {community}",
-        )
-    if unknown < 0:
-        total_str.append(
-            f'🔎 Under investigation: -{current["Caught COVID-19 within NZ, but source is unknown"] + current["Under investigation"]}'
-        )
     if border + community + unknown == recovered:
         return [
             "🦠 RECOVERY UPDATE",
